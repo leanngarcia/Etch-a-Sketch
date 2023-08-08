16c3b0c6815d2a3e2209f5ce3fdbe846dbc2407b
lean <leanndro.garcia@gmail.com>
2023-08-08 07:24:08 -0300

    dibujado de grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("holis")
+const containerGrid = document.getElementById("containerGrid");
+
+let sizeGrid = 64;
+
+function drawGrid(sizeGrid) {
+  for (let i = 0; i < sizeGrid; i++) {
+    const rowGrid = document.createElement("div");
+
+    rowGrid.classList.add("row-grid");
+
+    containerGrid.appendChild(rowGrid);
+
+    for(let z = 0; z < sizeGrid; z++){
+        const colGrid = document.createElement("div");
+
+        colGrid.classList.add("col-grid");
+
+        rowGrid.appendChild(colGrid);
+    }
+
+    
+  }
+}
+
+drawGrid(sizeGrid);
