3a914e8221ecc4429c5b0a813982506bf04575d9
lean <leanndro.garcia@gmail.com>
2023-08-09 00:36:43 -0300

    add button to paint black

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 const containerGrid = document.getElementById("containerGrid");
 const optionSize = document.getElementById("size");
+const gridUnit = document.getElementsByClassName("grid-unit");
+const btnBlack = document.getElementById("btnBlack");
 
 let sizeGrid = 64;
 
 drawGrid(sizeGrid);
 
+//add event to resize grid at select list
 optionSize.addEventListener("change", (event) => {
   deleteGrid(containerGrid);
 
   sizeGrid = event.target.value;
 
   drawGrid(sizeGrid);
+});
+
+//add event to paint grid on black
+btnBlack.addEventListener("click", () => {
+  for (const unit of gridUnit) {
+    unit.addEventListener("mouseover", () => {
+      unit.style.backgroundColor = "black";
+    });
+  }
 });
 
 //draw grid at specific size
@@ -26,12 +38,14 @@ function drawGrid(sizeGrid) {
       const colGrid = document.createElement("div");
 
       colGrid.classList.add("col-grid");
+      colGrid.classList.add("grid-unit");
 
       rowGrid.appendChild(colGrid);
     }
   }
 }
 
+//delete grid
 function deleteGrid(containerGrid) {
   while (containerGrid.firstChild) {
     containerGrid.removeChild(containerGrid.firstChild);
